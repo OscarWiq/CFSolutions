b261c02c56ad73f7b1f5eefd6c30fe11f13ee011
Oscar <owiqvist@gmail.com>
2019-04-18 15:46:13 +0200

    bufix 158A Next round

--- 158/A.c
+++ 158/A.c
@@ -3,12 +3,14 @@
 int main () {
 
     int n, k, count = 0;
-    int a[50];
+    int a[101];
     scanf("%d %d", &n, &k);
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
+    }
 
+    for (int i = 0; i < n; i++) {
         if (a[i] >= a[k - 1] && a[i] > 0) {
             count++;
         }
