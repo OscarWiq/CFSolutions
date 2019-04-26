398cbe23ac900bf290804d9e6a1be409fd36738a
Oscar <48216247+OscarWiq@users.noreply.github.com>
2019-04-26 21:08:36 +0200

    1157A bugfix and clean

--- 1157/A.c
+++ 1157/A.c
@@ -4,31 +4,17 @@
 
 typedef int64_t I64;
 
-I64 reachNumber(I64 n);
+int main() {
+	I64 n, count = 0;
+	scanf("%"SCNi64, &n);
 
-int main () {
-    
-    I64 n, count = 0;
-    scanf("%"SCNi64, &n);
-    
-    if (n >= 1 && n <= 9) {
-        printf("9\n");
-        return 0;
-    }
-
-    while (n > 1) {
-        n = reachNumber(n);
-        count++;
-    }
-
-    printf("%"PRIi64"\n", count + 1); 
-    return 0;
-}
-
-I64 reachNumber(I64 n) {
-    I64 res = n + 1;
-    while (res % 10 == 0) {
-        res /= 10;
-    }
-    return res;
+	while (n >= 10) {
+		n++;
+		while (n % 10 == 0)
+			n /= 10;
+		count++;
+	}
+	
+	printf("%d\n", count + 9);
+	return 0;
 }
