461fb3addd79171cbb3bbbae7fb221e271021641
Oscar <owiqvist@gmail.com>
2019-05-09 23:26:54 +0200

    1163A Eating soup

--- 1163/A.c
+++ 1163/A.c
@@ -5,6 +5,6 @@ int main () {
     int n, m;
     scanf("%d%d", &n, &m);
 
-    printf("%d\n", m == 0 ? 1 : m * 2 <= n ? m : n -m);
+    printf("%d\n", m == 0 ? 1 : m * 2 <= n ? m : n - m);
     return 0;
 }
